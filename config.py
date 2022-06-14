8b8bb96d6b50f74c853ef6d919b0c4da6e86e334
Grigoriy457 <77736651+Grigoriy457@users.noreply.github.com>
2022-06-14 12:11:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 HOST = "localhost"
 PORT = 5000
 
-TOKEN_1 = "OTgzMDIyODkzNTU2NDY1Njc1.GOXuag.58WgUxtckjIaD3EBUOolo-BpJRkVL8xruxfePk"
-TOKEN_2 = "OTgzMDY3MTQ0NDUzNDg4NzIx.G2xdKj.-FQHoe8_lungwNiBAI_3cP-q2rl8YE7jMqtS14"
-TOKEN_3 = "OTgzNjg5OTk1Njk1MDQ2NjY2.GMs3Lm.s3gLGrsoy5fUCGOna_mIvj4M0W1iyXARoeCqoo"
-
-
 ALPHABET = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 
 HELLO_TYPES = ["Привет", "Привет всем", "Прив", "Хай", "Здравствуй", "Hello", "Hello everyone", "Hi", "Hi everyone"]
